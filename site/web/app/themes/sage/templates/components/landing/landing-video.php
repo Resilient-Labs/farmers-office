0bee5eb24db1776eaa538807763e2af8899e1c0e
Malcolm Grant <malcolmgrant@Resilients-MacBook-Air.local>
2017-10-16 16:53:38 -0400

    landing video changes initial commit

--- site/web/app/themes/sage/templates/components/landing/landing-video.php
+++ site/web/app/themes/sage/templates/components/landing/landing-video.php
@@ -1,22 +1,24 @@
 <section class="landing-video">
     <?php // inspect this to see what element it renders as ?>
     <?php if (get_field('video')) : ?>
-        <?php the_field('video'); ?>
+    <div class="video"><?php the_field('video'); ?></div>
     <?php endif; ?>
 
     <ul>
         <?php if( have_rows('description') ): ?>
-            <?php while( have_rows('description') ): ?>
+            <?php while( have_rows('description') ): the_row();?>
                 <?php if (get_sub_field('bullet_point')): ?>
-                    <li>
+
+                    <li class="bullets">
                         <?php the_sub_field('bullet_point'); ?>
                     </li>
                 <?php endif; ?>
             <?php endwhile; ?>
         <?php endif; ?>
+        
     </ul>
 
     <?php if (get_field('brochure')) : ?>
-        <a href="<?php the_field('brochure'); ?>" download>Download Brochure</a>
-    <?php endif; ?>
+        <a href=" <?php the_field('brochure'); ?>" download>Download Brochure</a>
+    <?php endif; ?> 
 </section>
