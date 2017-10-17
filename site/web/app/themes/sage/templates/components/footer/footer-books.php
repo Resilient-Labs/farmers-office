82ee96c55222fe4be4b2add5dc0f3ea4a3c10036
Malcolm Grant <malcolmgrant@Resilients-MacBook-Air.local>
2017-10-17 17:09:04 -0400

    changes that muigai wanted me to make, no more classes

--- site/web/app/themes/sage/templates/components/footer/footer-books.php
+++ site/web/app/themes/sage/templates/components/footer/footer-books.php
@@ -3,28 +3,28 @@
         <?php while( have_rows('book') ): the_row(); ?>
             <div class="footer-book">
                 <?php if (get_sub_field('book_title')) : ?>
-                    <h3 class="title">
+                    <h3>
                         <?php the_sub_field('book_title'); ?>
                     </h3>
                 <?php endif; ?>
 
                 <?php if (get_sub_field('book_author')) : ?>
-                    <small class="author">
+                    <small>
                         <?php the_sub_field('book_author'); ?>
                     </small>
                 <?php endif; ?>
                 <?php if (get_sub_field('book_image')) : ?>
-                    <img class="img" src="<?php the_sub_field('book_image'); ?>" />
+                    <img src="<?php the_sub_field('book_image'); ?>" />
                 <?php endif; ?>
 
                 <?php if (get_sub_field('book_description')) : ?>
-                    <p class="descrip">
+                    <p>
                         <?php the_sub_field('book_description'); ?>
                     </p>
                 <?php endif; ?>
 
                 <?php if (get_sub_field('book_link')) : ?>
-                    <a class="link" href="<?php the_sub_field('book_link'); ?>">
+                    <a href="<?php the_sub_field('book_link'); ?>">
                         Buy Now
                     </a>
                 <?php endif; ?>
