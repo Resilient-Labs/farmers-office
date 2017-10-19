173965410e622cfe9ed984a66ce2d60ae520b34c
Sebastian Jaramillo <sebastianjaramillo@resilients-MacBook-Air-2.local>
2017-10-19 13:13:48 -0400

    Just Copied Muigai's shit but I understand it which is what matters the most

--- site/web/app/themes/sage/templates/components/landing/landing-welcome.php
+++ site/web/app/themes/sage/templates/components/landing/landing-welcome.php
@@ -1,9 +1,13 @@
 <section class="welcome">
-    <h2>Welcome</h2>
-    <p>
-    Whether you struggle to understand the economics of your business or just want to increase your farm’s profitability, this course is for you! Each week, you will delve into specific business topics that will help you maximize your farm’s profitability.
-    </p>
-    <img src="<?= get_template_directory_uri(); ?>/dist/images/welcome-1.jpg" />
-    <img src="<?= get_template_directory_uri(); ?>/dist/images/welcome-2.jpg" />
-    <img src="<?= get_template_directory_uri(); ?>/dist/images/welcome-3.jpg" />
+    <div>
+        <h2>Welcome</h2>
+        <p>
+            Whether you struggle to understand the economics of your business or just want to increase your farm’s profitability, this course is for you! Each week, you will delve into specific business topics that will help you maximize your farm’s profitability.
+        </p>
+    </div>
+    <div>
+        <img src="<?= get_template_directory_uri(); ?>/dist/images/welcome-1.jpg" />
+        <img src="<?= get_template_directory_uri(); ?>/dist/images/welcome-2.jpg" />
+        <img src="<?= get_template_directory_uri(); ?>/dist/images/welcome-3.jpg" />
+    </div>
 </section>
